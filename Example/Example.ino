e66c5876a3c31998f64c84754f32f403ddacdd9f
Joël <joel.wirz@erowa.com>
2024-11-17 15:29:02 +0100

    add example

---
+++ Example/Example.ino
@@ -0,0 +1,13 @@
+#include "Snowman.h"
+
+Snowman pizzaman(9, 10, 170, 50);
+
+void setup() {
+  // put your setup code here, to run once:
+  pizzaman.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pizzaman.loop();
+}
